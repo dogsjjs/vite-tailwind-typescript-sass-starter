88b1e0dadba87d16472064e75cd21de123b26756
dogsjjs <1428204486@qq.com>
2023-07-19 09:24:13 +0800

    代码初始化。

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-[100vh] flex items-center justify-center">
+    <span class="text-4xl text-blue-400">Vite + Tailwindcss + Typescript</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
